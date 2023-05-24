9a6dd12ca924578216e47061d4b5dcb1f3c099e1
Nero811 <129769018+Nero811@users.noreply.github.com>
2023-05-25 00:21:16 +0800

    Delete 242.Valid_Anagram directory

--- 242.Valid_Anagram/Valid_Anagram/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String s="anagram";
-        String t="nagarak";
-        if(sol.isAnagram(s,t))System.out.println("True");
-        else System.out.println("False");
-    }
-}
