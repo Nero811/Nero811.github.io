58beaa2857190d18300f53ec3276b961906624c2
Nero811 <jy811005@gmail.com>
2023-05-25 00:17:47 +0800

    update 242 java version

---
+++ 242.Valid_Anagram/Valid_Anagram/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s="anagram";
+        String t="nagarak";
+        if(sol.isAnagram(s,t))System.out.println("True");
+        else System.out.println("False");
+    }
+}
