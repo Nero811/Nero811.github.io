5d7aa2d9faf4e090626c26cac6d84c804650174e
Nero811 <jy811005@gmail.com>
2023-09-05 21:56:58 +0800

    2023/09/05 update

---
+++ Leetcode/459.Repeated_Substring_Pattern/Java/Repeated_Substring_Pattern2/Repeated_Substring_Pattern/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Author:Jack Yang
+ * Date:2023/09/05
+ * Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "abcabcabcabc";
+        //String s = "aba";
+        System.out.println(sol.repeatedSubstringPattern(s));
+    }
+}
