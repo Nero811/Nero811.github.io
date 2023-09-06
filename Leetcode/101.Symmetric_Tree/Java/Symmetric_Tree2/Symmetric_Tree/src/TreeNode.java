9ab758cc1caa1ca2b0ee511d6cf74bbaf9778ce8
Nero811 <jy811005@gmail.com>
2023-09-06 20:15:55 +0800

    2023/09/06 update

---
+++ Leetcode/101.Symmetric_Tree/Java/Symmetric_Tree2/Symmetric_Tree/src/TreeNode.java
@@ -0,0 +1,21 @@
+/**
+ * Author:Jack Yang
+ * Date:2023/09/06
+ * Description:
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
