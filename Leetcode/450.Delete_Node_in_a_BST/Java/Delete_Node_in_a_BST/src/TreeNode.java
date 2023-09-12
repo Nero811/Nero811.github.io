c65a53d7be5332a5dca7016cbbe9f663bbb4432e
Nero811 <jy811005@gmail.com>
2023-09-12 21:26:16 +0800

    2023/09/12 update

---
+++ Leetcode/450.Delete_Node_in_a_BST/Java/Delete_Node_in_a_BST/src/TreeNode.java
@@ -0,0 +1,21 @@
+/**
+ * Author:Jack Yang
+ * Date:2023/09/12
+ * Description:
+ */
+public class TreeNode {
+    protected int val;
+    protected TreeNode left;
+    protected TreeNode right;
+    TreeNode(){}
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
